088dbf1c50111284f780f98219acd013f38a73a7
vitalien-bytes <contact@digitaltelecomnetwork.fr>
2025-11-11 00:24:08 +0100

    Update database.py

--- database.py
+++ database.py
@@ -4,6 +4,11 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+# forcer SQLAlchemy à utiliser le driver pg8000
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+pg8000://")
+
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
